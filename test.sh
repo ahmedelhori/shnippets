020ff5ed9db00a981aa078c8d6f4e5e39bd241da
Ahmed Elhori <dev@elhori.com>
2021-07-17 21:43:59 +0200

    Removed unnecessary check

--- test.sh
+++ test.sh
@@ -2,11 +2,6 @@
 
 for file in ./src/main/*.sh; do
 	shellcheck "$file" || exit 1
-
-	if ! sed -n '/^#!\/bin\/sh/q' "$file"; then
-		echo 'Error: Shebang not found on first line.'
-		exit 1
-	fi
 done
 
 for file in ./src/test/*.sh; do
